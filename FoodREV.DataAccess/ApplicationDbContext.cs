e102988427d3a5317e8d88809f4dbe6cf1f4032b
MEHEDI <me.mehedi@hotmail.com>
2020-06-22 13:05:46 +0600

    add-migration AddOrderHeader&DetailsToDB

--- FoodREV.DataAccess/ApplicationDbContext.cs
+++ FoodREV.DataAccess/ApplicationDbContext.cs
@@ -20,5 +20,7 @@ namespace FoodREV.DataAccess
         public DbSet<MenuItem> MenuItem { get; set;}
         public DbSet<ApplicationUser> ApplicationUser { get; set;}
         public DbSet<ShoppingCart> ShoppingCart { get; set;}
+        public DbSet<OrderHeader> OrderHeader { get; set;}
+        public DbSet<OrderDetails> OrderDetails { get; set;}
     }
 }
